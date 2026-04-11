c41b48ecbabfa4824e1911ec1f1b9944c87fb7ca
XvXkirinXvX <irfanrein1@gmail.com>
2026-04-11 19:57:51 +0700

    Change autosend interval to 1 hour
    Updated the autosend interval from 10 seconds to 1 hour.

--- utils/autosend.js
+++ utils/autosend.js
@@ -22,7 +22,7 @@ function startAutoMessage(client, channelId) {
     } catch (err) {
       console.error("Auto message error:", err);
     }
-  }, 10 * 1000); // 10 seconds test
+  }, 60 * 60 * 1000); // 10 seconds test
 
   return true;
 }
